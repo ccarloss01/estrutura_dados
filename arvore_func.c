3e4eea77fd3feea77633b1af08b3b2e0da01e394
luankaio <luankaiosilveira45@hotmail.com>
2025-02-16 15:43:30 -0300

    check de malloc retorna null

--- arvore_func.c
+++ arvore_func.c
@@ -14,6 +14,10 @@
 struct arv_pessoa *inserir_arvore_pessoa_codigo(struct arv_pessoa *raiz, struct pessoa *pessoa) {
     if (raiz == NULL) {
         struct arv_pessoa *novo = malloc(sizeof(struct arv_pessoa));
+        if (novo == NULL) {
+            fprintf(stderr, "Erro de alocacao\n");
+            exit(EXIT_FAILURE);
+        }
         novo->pessoa = pessoa;
         novo->dir = NULL;
         novo->esq = NULL;
@@ -22,7 +26,7 @@ struct arv_pessoa *inserir_arvore_pessoa_codigo(struct arv_pessoa *raiz, struct 
 
     if (atoi(pessoa->codigo) > atoi(raiz->pessoa->codigo)) {
         raiz->dir = inserir_arvore_pessoa_codigo(raiz->dir, pessoa);
-    } else if(atoi(pessoa->codigo) < atoi(raiz->pessoa->codigo)) {
+    } else if (atoi(pessoa->codigo) < atoi(raiz->pessoa->codigo)) {
         raiz->esq = inserir_arvore_pessoa_codigo(raiz->esq, pessoa);
     }
 
@@ -32,6 +36,10 @@ struct arv_pessoa *inserir_arvore_pessoa_codigo(struct arv_pessoa *raiz, struct 
 struct arv_pessoa *inserir_arvore_pessoa_nome(struct arv_pessoa *raiz, struct pessoa *pessoa) {
     if (raiz == NULL) {
         struct arv_pessoa *novo = malloc(sizeof(struct arv_pessoa));
+        if (novo == NULL) {
+            fprintf(stderr, "Erro de alocacao\n");
+            exit(EXIT_FAILURE);
+        }
         novo->pessoa = pessoa;
         novo->dir = NULL;
         novo->esq = NULL;
@@ -40,7 +48,7 @@ struct arv_pessoa *inserir_arvore_pessoa_nome(struct arv_pessoa *raiz, struct pe
 
     if (strcmp(pessoa->nome, raiz->pessoa->nome) > 0) {
         raiz->dir = inserir_arvore_pessoa_nome(raiz->dir, pessoa);
-    } else if(strcmp(pessoa->nome, raiz->pessoa->nome) < 0) {
+    } else if (strcmp(pessoa->nome, raiz->pessoa->nome) < 0) {
         raiz->esq = inserir_arvore_pessoa_nome(raiz->esq, pessoa);
     }
 
@@ -48,29 +56,20 @@ struct arv_pessoa *inserir_arvore_pessoa_nome(struct arv_pessoa *raiz, struct pe
 }
 
 void transf_lista_arvore_pessoa_codigo(struct arv_pessoa **raiz, struct pessoa *lista) {
-
     while (lista != NULL) {
-
         *raiz = inserir_arvore_pessoa_codigo(*raiz, lista);
         lista = lista->prox;
-
     }
-
 }
 
 void transf_lista_arvore_pessoa_nome(struct arv_pessoa **raiz, struct pessoa *lista) {
-
     while (lista != NULL) {
-
         *raiz = inserir_arvore_pessoa_nome(*raiz, lista);
         lista = lista->prox;
-
     }
-
 }
 
 void imprimir_pessoa(struct pessoa *pessoa) {
-
     printf("Código: %s", pessoa->codigo);
     printf("\n- %s", pessoa->nome);
     printf("\n- %s", pessoa->fone);
@@ -91,6 +90,10 @@ void imprimir_arvore_pessoa(struct arv_pessoa *raiz) {
 struct arv_tipo_de_pet *inserir_arvore_tipo_pet_codigo(struct arv_tipo_de_pet *raiz, struct tipo_de_pet *tipo_de_pet) {
     if (raiz == NULL) {
         struct arv_tipo_de_pet *novo = malloc(sizeof(struct arv_tipo_de_pet));
+        if (novo == NULL) {
+            fprintf(stderr, "Erro de alocacao\n");
+            exit(EXIT_FAILURE);
+        }
         novo->tipo_de_pet = tipo_de_pet;
         novo->dir = NULL;
         novo->esq = NULL;
@@ -99,7 +102,7 @@ struct arv_tipo_de_pet *inserir_arvore_tipo_pet_codigo(struct arv_tipo_de_pet *r
 
     if (atoi(tipo_de_pet->codigo) > atoi(raiz->tipo_de_pet->codigo)) {
         raiz->dir = inserir_arvore_tipo_pet_codigo(raiz->dir, tipo_de_pet);
-    } else if(atoi(tipo_de_pet->codigo) < atoi(raiz->tipo_de_pet->codigo)) {
+    } else if (atoi(tipo_de_pet->codigo) < atoi(raiz->tipo_de_pet->codigo)) {
         raiz->esq = inserir_arvore_tipo_pet_codigo(raiz->esq, tipo_de_pet);
     }
 
@@ -109,6 +112,10 @@ struct arv_tipo_de_pet *inserir_arvore_tipo_pet_codigo(struct arv_tipo_de_pet *r
 struct arv_tipo_de_pet *inserir_arvore_tipo_pet_descricao(struct arv_tipo_de_pet *raiz, struct tipo_de_pet *tipo_de_pet) {
     if (raiz == NULL) {
         struct arv_tipo_de_pet *novo = malloc(sizeof(struct arv_tipo_de_pet));
+        if (novo == NULL) {
+            fprintf(stderr, "Erro de alocacao\n");
+            exit(EXIT_FAILURE);
+        }
         novo->tipo_de_pet = tipo_de_pet;
         novo->dir = NULL;
         novo->esq = NULL;
@@ -117,7 +124,7 @@ struct arv_tipo_de_pet *inserir_arvore_tipo_pet_descricao(struct arv_tipo_de_pet
 
     if (strcmp(tipo_de_pet->descricao, raiz->tipo_de_pet->descricao) > 0) {
         raiz->dir = inserir_arvore_tipo_pet_descricao(raiz->dir, tipo_de_pet);
-    } else if(strcmp(tipo_de_pet->descricao, raiz->tipo_de_pet->descricao) < 0) {
+    } else if (strcmp(tipo_de_pet->descricao, raiz->tipo_de_pet->descricao) < 0) {
         raiz->esq = inserir_arvore_tipo_pet_descricao(raiz->esq, tipo_de_pet);
     }
 
@@ -125,32 +132,22 @@ struct arv_tipo_de_pet *inserir_arvore_tipo_pet_descricao(struct arv_tipo_de_pet
 }
 
 void transf_lista_arvore_tipo_pet_codigo(struct arv_tipo_de_pet **raiz, struct tipo_de_pet *lista) {
-
     while (lista != NULL) {
-
         *raiz = inserir_arvore_tipo_pet_codigo(*raiz, lista);
         lista = lista->prox;
-
     }
-
 }
 
 void transf_lista_arvore_tipo_pet_descricao(struct arv_tipo_de_pet **raiz, struct tipo_de_pet *lista) {
-
     while (lista != NULL) {
-
         *raiz = inserir_arvore_tipo_pet_descricao(*raiz, lista);
         lista = lista->prox;
-
     }
-
 }
 
 void imprimir_tipo_pet(struct tipo_de_pet *tipo_pet) {
-
     printf("Código: %s", tipo_pet->codigo);
     printf("\n- %s\n", tipo_pet->descricao);
-
 }
 
 void imprimir_arvore_tipo_pet(struct arv_tipo_de_pet *raiz) {
@@ -166,6 +163,10 @@ void imprimir_arvore_tipo_pet(struct arv_tipo_de_pet *raiz) {
 struct arv_pet *inserir_arvore_pet_codigo(struct arv_pet *raiz, struct pet *pet) {
     if (raiz == NULL) {
         struct arv_pet *novo = malloc(sizeof(struct arv_pet));
+        if (novo == NULL) {
+            fprintf(stderr, "Erro de alocacao\n");
+            exit(EXIT_FAILURE);
+        }
         novo->pet = pet;
         novo->dir = NULL;
         novo->esq = NULL;
@@ -174,7 +175,7 @@ struct arv_pet *inserir_arvore_pet_codigo(struct arv_pet *raiz, struct pet *pet)
 
     if (atoi(pet->codigo) > atoi(raiz->pet->codigo)) {
         raiz->dir = inserir_arvore_pet_codigo(raiz->dir, pet);
-    } else if(atoi(pet->codigo) < atoi(raiz->pet->codigo)) {
+    } else if (atoi(pet->codigo) < atoi(raiz->pet->codigo)) {
         raiz->esq = inserir_arvore_pet_codigo(raiz->esq, pet);
     }
 
@@ -184,6 +185,10 @@ struct arv_pet *inserir_arvore_pet_codigo(struct arv_pet *raiz, struct pet *pet)
 struct arv_pet *inserir_arvore_pet_nome(struct arv_pet *raiz, struct pet *pet) {
     if (raiz == NULL) {
         struct arv_pet *novo = malloc(sizeof(struct arv_pet));
+        if (novo == NULL) {
+            fprintf(stderr, "Erro de alocacao\n");
+            exit(EXIT_FAILURE);
+        }
         novo->pet = pet;
         novo->dir = NULL;
         novo->esq = NULL;
@@ -192,7 +197,7 @@ struct arv_pet *inserir_arvore_pet_nome(struct arv_pet *raiz, struct pet *pet) {
 
     if (strcmp(pet->nome, raiz->pet->nome) > 0) {
         raiz->dir = inserir_arvore_pet_nome(raiz->dir, pet);
-    } else if(strcmp(pet->nome, raiz->pet->nome) < 0) {
+    } else if (strcmp(pet->nome, raiz->pet->nome) < 0) {
         raiz->esq = inserir_arvore_pet_nome(raiz->esq, pet);
     }
 
@@ -200,34 +205,24 @@ struct arv_pet *inserir_arvore_pet_nome(struct arv_pet *raiz, struct pet *pet) {
 }
 
 void transf_lista_arvore_pet_codigo(struct arv_pet **raiz, struct pet *lista) {
-
     while (lista != NULL) {
-
         *raiz = inserir_arvore_pet_codigo(*raiz, lista);
         lista = lista->prox;
-
     }
-
 }
 
 void transf_lista_arvore_pet_nome(struct arv_pet **raiz, struct pet *lista) {
-
     while (lista != NULL) {
-
         *raiz = inserir_arvore_pet_nome(*raiz, lista);
         lista = lista->prox;
-
     }
-
 }
 
 void imprimir_pet(struct pet *pet) {
-
     printf("Código: %s", pet->codigo);
     printf("\n- %s", pet->nome);
     printf("\n- %s", pet->codigo_pes);
     printf("\n- %s\n", pet->codigo_tipo);
-
 }
 
 void imprimir_arvore_pet(struct arv_pet *raiz) {
