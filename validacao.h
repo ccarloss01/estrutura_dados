60cdd10ff88953b69c98e619007c04f904dbb651
carlos <carlosmagno369@gmail.com>
2025-02-15 23:04:50 -0300

    adicionando declaracao de funcoes

--- validacao.h
+++ validacao.h
@@ -5,4 +5,20 @@ int validar_instrucao(struct comando *comando, char tipo[]);
 
 char *validar_tabela(char *inst);
 
+int validar_insert(struct comando *cmd);
+
+int validar_select(struct comando *cmd);
+
+int validar_delete(struct comando *cmd);
+
+int validar_update(struct comando *cmd);
+
+int validar_comando(struct comando *cmd);
+
+int validar_campo(const char *tabela, const char *campo);
+
+int extrair_campos(struct comando *cmd, char tabela[][255]);
+
+int extrair_valores(struct comando *cmd, char tabela[][255]);
+
 #endif
